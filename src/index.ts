d5bfa82d69b7d4e46141afc4068935dfa7c53bbe
Daniel Ramirez <contact@dann.dev>
2022-09-12 15:24:52 -0500

    Start: Project initializated

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const names:string = '1';
+const secondNames:string = '2';
+
+console.log("🚀 ~ file: index.ts ~ line 2 ~ names", names)
+console.log("🚀 ~ file: index.ts ~ line 3 ~ secondNames", secondNames)
