eb0fa1373eccda1f04796f32d0c4db82bd324965
Daniel Ramirez <contact@dann.dev>
2022-09-13 23:12:06 -0500

    add: Firts test to test main route from API

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,25 @@
+import supertest from 'supertest';
+import {app, server} from '../index';
+const request = supertest(app);
+
+afterAll(() => {
+  server.close();
+});
+
+describe('Test endpoint responses', () => {
+  it('gets the / endpoint', async () => {
+    const response = await request.get('/api');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      response: 'ok',
+      status: 200,
+      routes: [
+        {
+          metod: 'GET',
+          path: '/api/',
+          description: 'API Working',
+        },
+      ],
+    });
+  });
+});
