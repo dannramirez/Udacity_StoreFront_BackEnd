157843e88b4d8679727558fcdefa4dfb347e78c7
Daniel Ramirez <contact@dann.dev>
2022-09-15 13:36:41 -0500

    fix: migration userTable an uudi-extension

--- migrations/sqls/20220914193350-uuidPG-down.sql
+++ migrations/sqls/20220914193350-uuidPG-down.sql
@@ -1,1 +1,1 @@
-/* Replace with your SQL commands */
+DROP EXTENSION IF EXISTS "uuid-ossp";
--- migrations/sqls/20220914193405-usersTable-up.sql
+++ migrations/sqls/20220914193405-usersTable-up.sql
@@ -1,11 +1,8 @@
 CREATE TABLE IF NOT EXISTS "User" (
     "id" uuid DEFAULT uuid_generate_v1 (),
-    "userName" VARCHAR(25) NOT NULL,
-    "nombre" VARCHAR(25) NOT NULL,
+    "firstName" VARCHAR(25) NOT NULL,
+    "lastName" VARCHAR(25) NOT NULL,
     "password" TEXT NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
-    "email" VARCHAR(50) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
     PRIMARY KEY ("id")
 );
