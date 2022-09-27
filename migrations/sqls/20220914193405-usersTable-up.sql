b041199b0ed135ac2114850b4bf917157bbe2ca4
Daniel Ramirez <contact@dann.dev>
2022-09-26 21:00:56 -0500

    fix: SQL Users table name

--- migrations/sqls/20220914193405-usersTable-up.sql
+++ migrations/sqls/20220914193405-usersTable-up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "User" (
+CREATE TABLE IF NOT EXISTS "Users" (
     "id" uuid DEFAULT uuid_generate_v1 (),
     "firstName" VARCHAR(25) NOT NULL,
     "lastName" VARCHAR(25) NOT NULL,
