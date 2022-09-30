1031f4a6cf56fd102c43efe235dd2fe7aef28b9b
Daniel Ramirez <contact@dann.dev>
2022-09-30 08:22:40 -0500

    fix: types on hash password function

--- src/utilities/hash_validate_password.ts
+++ src/utilities/hash_validate_password.ts
@@ -4,15 +4,15 @@ dotenv.config();
 
 const {BCRYPY_PASSWORD: pepper, SALT_ROUNDS: saltRounds} = process.env;
 
-const salt = bcrypt.genSaltSync(parseInt(saltRounds!, 10));
+const salt = bcrypt.genSaltSync(parseInt(saltRounds as string, 10));
 
 const hashPassword = (password: string) => {
   console.log(salt);
-  return bcrypt.hashSync(password + pepper, salt);
+  return bcrypt.hashSync(`${password}${pepper as string}`, salt);
 };
 
 const validatePassword = (password: string, password_digest: string) => {
-  return bcrypt.compareSync(password + pepper, password_digest);
+  return bcrypt.compareSync(`${password}${pepper as string}`, password_digest);
 };
 
 export {hashPassword, validatePassword};
