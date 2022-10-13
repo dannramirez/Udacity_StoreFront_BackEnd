6872f0c1829adb12909b610d48ef7f6cc9cdb2b4
Daniel Ramirez <contact@dann.dev>
2022-10-13 15:02:11 -0500

    Fix: Requirements and increase sql size for name column

--- migrations/sqls/20220915042705-productsTable-up.sql
+++ migrations/sqls/20220915042705-productsTable-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "Products"(
     "id" uuid DEFAULT uuid_generate_v1(),
-    "name" VARCHAR(25) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "category" VARCHAR(25) NOT NULL,
 
